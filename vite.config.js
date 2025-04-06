92f750ffc568b06c6d8535f2d1ebd60336ad0818
Harsh Yadav <harshyadav12012004@gmail.com>
2025-04-07 01:05:37 +0530

    updated the package.json and cite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import compression from 'vite-plugin-compression2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    react(),
-    compression({
-      algorithm: 'gzip',
-      exclude: [/\.(br)$/, /\.(gz)$/],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
